7f056bc46917202e4fd0c0b8b159f7352ff7e6d1
BUITHIHUYEN <huyenbui2345@gmail.com>
2022-01-25 08:31:02 +0700

    upbai

--- bai9_aop_va_exception_handling/bai_tap/ung_dung_muon_sach/src/main/java/com/example/ung_dung_muon_sach/service/ICodeService.java
+++ bai9_aop_va_exception_handling/bai_tap/ung_dung_muon_sach/src/main/java/com/example/ung_dung_muon_sach/service/ICodeService.java
@@ -2,10 +2,9 @@ package com.example.ung_dung_muon_sach.service;
 
 import com.example.ung_dung_muon_sach.model.Code;
 
-import java.util.Optional;
 
 public interface ICodeService {
-    void create(Code code);
+    void save(Code code);
     void delete (Code code);
     Code findByCode(Long code);
 }
